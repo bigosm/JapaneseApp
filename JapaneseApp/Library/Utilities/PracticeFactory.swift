01d8f4883482327960fdcafe5636e2db0524d4f5
Michał Bigos <michal.bigos@hemmersbach.com>
2020-02-10 16:15:31 +0100

    Clean up app structure

---
+++ JapaneseApp/Library/Utilities/PracticeFactory.swift
@@ -0,0 +1,13 @@
+//
+//  PracticeFactory.swift
+//  JapaneseApp
+//
+//  Created by Michal Bigos on 10/02/2020.
+//  Copyright © 2020 Example. All rights reserved.
+//
+
+import Foundation
+
+internal struct PracticeFactory {
+    
+}
